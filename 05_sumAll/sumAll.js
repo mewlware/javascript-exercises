e264e09c4c5a76124042319c1b61b30192575bab
MEL <65815530+mewlware@users.noreply.github.com>
2022-02-09 07:06:33 +0800

    Add passing solution for 05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numX, numY) {
+    if (typeof numX !== 'number' || typeof numY !== 'number') {
+        return 'ERROR';
+    } else if (numX < 0 || numY < 0) {
+        return 'ERROR';
+    }
+    
+    let sum = 0;
+    let start = numX;
+    let end = numY;
 
+    if (numX > numY) {
+        start = numY;
+        end = numX;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
