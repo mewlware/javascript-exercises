91e06b132efd58854fb8651590aa609fcb314b55
MEL <65815530+mewlware@users.noreply.github.com>
2022-02-09 06:28:11 +0800

    Add passing solution for 03 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    charArray = string.split('');
+    charArray.reverse();
+    return charArray.join('');
 };
 
 // Do not edit below this line
