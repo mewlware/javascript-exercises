97293f791c55f11d9fca2e181a004fdd1f8fbc32
MEL <65815530+mewlware@users.noreply.github.com>
2022-02-09 06:55:49 +0800

    Add passing solution for 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    let newArray = []
 
+    for (let item of array) {
+        let isItemRemoved = 0;
+        for (let itemToRemove of itemsToRemove) {
+            if (item === itemToRemove) {
+                isItemRemoved = 1;
+                break;
+            }
+        }
+        if (!isItemRemoved){
+            newArray.push(item);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
